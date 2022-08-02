92e5b9c80a3ff1f577b2ce1354617a1535144024
dciro8 <inforfercho@gmail.com>
2022-08-02 10:39:16 -0500

    Refactor espacios

--- Transenvios.Shipping.Api/Domains/ShipmentOrderService/ShipmentOrderPage/ShipmentCityProcessor.cs
+++ Transenvios.Shipping.Api/Domains/ShipmentOrderService/ShipmentOrderPage/ShipmentCityProcessor.cs
@@ -7,13 +7,11 @@ namespace Transenvios.Shipping.Api.Domains.ShipmentOrderService.ShipmentOrderPag
         private readonly IGetShipmentCity _getShipmentCity;
         private readonly IMapper _mapper;
 
-        public ShipmentCityProcessor(IMapper mapper,
-        IGetShipmentCity getShipmentCity)
+        public ShipmentCityProcessor(IMapper mapper,IGetShipmentCity getShipmentCity)
         {
             _getShipmentCity= getShipmentCity ??
                 throw new ArgumentNullException(nameof(getShipmentCity));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-
         }
 
         public async Task<IList<ShipmentCity>> GetShipmentCityAsync()
@@ -22,6 +20,5 @@ namespace Transenvios.Shipping.Api.Domains.ShipmentOrderService.ShipmentOrderPag
             var cities = _mapper.Map<IList<ShipmentCity>>(response);
             return cities;
         }
-
     }
 }
