6bdded5e796589924687795f7b421404a31b6d28
ediazconnectplanet <ediaz.connectplanet@gmail.com>
2023-01-20 20:04:28 -0500

    CP23: Update Shipment request and response for update order
    Update Shipment request and response for update order

--- Transenvios.Shipping.Api/Domains/ShipmentOrderService/Responses/ShipmentOrderResponse.cs
+++ Transenvios.Shipping.Api/Domains/ShipmentOrderService/Responses/ShipmentOrderResponse.cs
@@ -7,5 +7,12 @@ namespace Transenvios.Shipping.Api.Domains.ShipmentOrderService.Responses
         public long? Id { get; set; }
         public HttpStatusCode ResultCode { get; set; } = HttpStatusCode.OK;
         public string? ErrorMessage { get; set; }
+
+        public ShipmentOrderResponse Configure(HttpStatusCode resultCode, string? errorMessage)
+        {
+            ResultCode = resultCode;
+            ErrorMessage = errorMessage;
+            return this;
+        }
     }
 }
